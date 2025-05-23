e86387d148fa6d631067f092b24627a3c93389c0
Oltjona Gjyriqi <oltjonagjyriqi@gmail.com>
2025-05-23 20:57:41 +0900

    fixed the searchbar

--- frontend-app/src/app/components/navbar/navbar.component.ts
+++ frontend-app/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink }   from '@angular/router';
-import { RouterModule } from '@angular/router'; 
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  showSearch = false;
+  query = '';
+  results: any[] = [];
+
+  constructor(private api: ApiService) {}
+
+  toggleSearch() {
+    this.showSearch = true;
+    this.query = '';
+
+    setTimeout(() => {
+      const inputElement = document.getElementById('navbar-search-input');
+      inputElement?.focus();
+    }, 0);
+  }
+
+  submit() {
+    this.api.search(this.query).subscribe(data => {
+      this.results = data;
+      console.log("Search results:", data);
+    });
+  }
+}
