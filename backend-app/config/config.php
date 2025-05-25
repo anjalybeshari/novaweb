f72856addd05e81cc9cc7e71a86344ea5d954cfc
Oltjona Gjyriqi <oltjonagjyriqi@gmail.com>
2025-05-25 18:28:07 +0900

    .

--- backend-app/config/config.php
+++ backend-app/config/config.php
@@ -9,7 +9,6 @@ $port = 3306;
 $conn = new mysqli($host, $user, $password, $database, $port);
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
-    die(json_encode(["error" => "Database connection failed: " . $conn->connect_error]));
 }
 
 return $conn;
