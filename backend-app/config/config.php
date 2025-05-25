aa3992f5769be5bbb614fdc57efd0c843aa6b2f5
Oltjona Gjyriqi <oltjonagjyriqi@gmail.com>
2025-05-25 15:29:57 +0900

    cart service. pls dont forget to change config.php

--- backend-app/config/config.php
+++ backend-app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "Anja21/05/18";
+$host = "192.168.1.210";
+$user = "olta";
+$password = "olta_pw";
 $database = "NOVA";
+$port = 3306;
 
-$conn = new mysqli($host, $user, $password, $database);
-
+$conn = new mysqli($host, $user, $password, $database, $port);
 if ($conn->connect_error) {
     die("Connection failed: ". $conn->connect_error);
 }
