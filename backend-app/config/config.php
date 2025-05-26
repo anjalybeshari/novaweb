4ad8545ace22125093c5f25373fab603e4190d09
ani <ani.kodheli90@gmial.com>
2025-05-26 18:11:58 +0200

    fixed home page

--- backend-app/config/config.php
+++ backend-app/config/config.php
@@ -1,13 +1,14 @@
 <?php
 // backend-app/includes/connect.php
 
-$host = "localhost";
-$user = "root";
-$password = "Ani.0399";
+$host = "192.168.1.184";
+$user = "altea";
+$password = "altea_pw";
 $database = "NOVA";
+$port = 3306;
 
 // Përdorim $con në vend të $conn për konsistencë me funksionet
-$con = new mysqli($host, $user, $password, $database);
+$con = new mysqli($host, $user, $password, $database, $port);
 
 if ($con->connect_error) {
     die('Connection failed: ' . $con->connect_error);
